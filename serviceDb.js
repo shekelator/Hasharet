fc4beaadf92abe657fe023307dab97a36188f448
David Nichol <david_r_nichol@yahoo.com>
2014-03-30 17:37:40 -0400

    Added heavyweight unit tests and updates to service DB

--- serviceDb.js
+++ serviceDb.js
@@ -1,5 +1,7 @@
 var MongoClient = require("mongodb").MongoClient;
 var format = require("util").format;
+var _ = require("lodash-node");
+var moment = require("moment");
 
 var db = {
 	getServices: function(currentDate, callback) {
@@ -16,10 +18,45 @@ var db = {
 						return serviceDate.isAfter(currentDate);
 					});
 
-					callback(err, upcomingShabbatot);
+					callback(err, results);
 				});
 
 			});
+	},
+
+	setService: function(service, callback) {
+		var date = service.date;
+		var title = service.title;
+		MongoClient.connect("mongodb://127.0.0.1/services", function(err, db) {
+			if(err) { throw err; }
+
+				db.collection("services")
+					.update({"date": date, "title": title}, service,{w:1, multi: false, upsert: true}, function(err, objects) {
+					if(err) {
+						console.warn(err.message);
+					}
+
+					callback(err, objects);
+				});
+			});
+	},
+
+	deleteService: function(service, callback) {
+		var date = service.date;
+		var title = service.title;
+		MongoClient.connect("mongodb://127.0.0.1/services", function(err, db) {
+			if(err) { throw err; }
+
+				db.collection("services")
+					.remove({"date": date, "title": title}, {w:1}, function(err, objects) {
+					if(err) {
+						console.warn(err.message);
+					}
+
+					callback(err, objects);
+				});
+ 			});
+
 	}
 };
 
--- specs/heavyweight/serviceDbSpec.js
+++ specs/heavyweight/serviceDbSpec.js
@@ -1,12 +1,52 @@
-var serviceDb = require("../serviceDb");
+var serviceDb = require("../../serviceDb");
 
 describe("serviceDb", function() {
-	it("returns dates", function(done) {
-		serviceDb.getServices("2013-06-07", function(err, data) {
-			expect(data).not.toBeUndefined();
-			expect(data[0].date).toBe("2013-01-05");
-			expect(data[0].title).toBe("Parashat Shemot");
+	var testDate = new Date(2014, 8, 27);
+
+	var newService = {
+		date: testDate,
+		type: "shabbat",
+		title: "Vayiqra"
+	};
+
+	beforeEach(function(done) {
+		serviceDb.deleteService({date: newService.date, title: newService.title}, done);
+	});
+
+	it("returns empty list", function(done) {
+		serviceDb.getServices(testDate, function(err, results) {
+			if(err) throw err;
+
+			expect(results.length).toBe(0);
 			done();
 		});
 	});
+	
+	it("returns list of 1 after insert", function(done) {
+		
+		var getAndAssert = function() {
+			serviceDb.getServices(testDate, function(err, results) {
+				if(err) throw err;
+
+				expect(results.length).toBe(1);
+				done();
+			});
+		};
+		serviceDb.setService(newService, getAndAssert);
+
+	});
+
+	it("deletes services", function(done) {
+		serviceDb.setService(newService, function(err, objects) {
+			serviceDb.deleteService(newService, function(err, objects) {
+				if(err) throw err;
+
+				serviceDb.getServices(newService, function(err, results) {
+					expect(results.length).toBe(0);
+					done();
+				});
+			});
+		});
+	});
+});
 
